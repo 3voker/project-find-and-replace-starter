f5fb72ed0a535841dd153fdc371b493ddf8cee92
3voker <Drechristophe@gmail.com>
2020-06-11 18:46:39 -0500

    Added changes couldn't get it to appear in the proper field.

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 // You should NOT change the HTML or CSS in this project (at least until you reach
 // the bonus objectives). Focus on the JavaScript.
 
-const findInput = document.querySelector(".find-input")
+const findInput = document.querySelector(".find-input");
+
+
 const replaceInput = document.querySelector(".replace-input")
 const replaceAllButton = document.querySelector(".replace-all-button")
 
@@ -10,19 +12,60 @@ const replaceAllButton = document.querySelector(".replace-all-button")
 // this array.
 const rowElements = document.querySelectorAll(".row")
 
-// When you call the function belwo, it will get and return an INNER ARRAY
+const cellElements = document.querySelectorAll(".cell")
+
+let rowElementsArray = [rowElements [cellElements]];
+// When you call the function below, it will get and return an INNER ARRAY
 // containing the cell elements for a given row.
 // Call this function from WITHIN your row elements loop. Then you will, in turn,
 // need to loop over the resulting cell elements. But where should this whole
 // NESTED LOOP go? Think through the user's experience: when should WHAT happen? 
-function getCellElements (currentRowElement) {
+function getCellElements (currentRowElement)
+{
     return currentRowElement.querySelectorAll(".cell")
 }
 
+replaceAllButton.addEventListener("click", function()
+{
+    //Find the Input using .find-input...
+  //Try to get the findInput, that's probably what's not working here...
+    //Hook up the array. 
+    //Find where in the array is the Element I need. 
+  
+   let findText = findInput.value;
 
-// YOUR CODE GOES HERE
+   let replaceText = replaceInput.value;
 
+    let numberInCell;
 
+   console.log(findText);
+   if(rowElements != null)
+   {
+    //Toggle through the rowElements
+    for(childrenInRowElements = 0, childrenInRowElements < rowElements.length; childrenInRowElements++;)
+        { 
+      //Create array that gets children elements using getCellElements 
+          let arrayOfCells = getCellElements(rowElements[childrenInRowElements]);
+            //toggle through the cell elements
+            for(cell = 0, cell < arrayOfCells.length; cell++;)
+            {
+                //Find if the element
+                if(arrayOfCells[cell].innerHTML.includes(findText))
+                {
+                    //Initialize variable to assign to array place and using .replace for findText to ReplaceText
+                    let replaceItem = arrayOfCells[cell].innerHTML.replace(findText, replaceText);
+                    //Assign arrayOfCell position .innerHTML to replace Item.
+                    arrayOfCells[cell].innerHTML =  Text(replaceItem);
+                    arrayOfCells[cell].innerHTML += replaceItem.innerHTML; 
+                }
+            }
+        }
+   }
+    //replace the input using .replace-input...
+    //return replaced input in the location of the found input..
+
+    //Assign elementINeed to currentRowElement
+})
 // One last thing: dedicate very careful attention to using variables and
 // naming them accurately.
 // And when you change the value you are assigning to a variable, don't
